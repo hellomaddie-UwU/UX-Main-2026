1266ae94d8bc4ba8dcdbe2562e693790c92a441d
hellomaddieUwU <info@hellomaddie.com>
2026-04-14 15:26:46 +0800

    added horizontal scrolling effect

---
+++ script.js
@@ -0,0 +1,26 @@
+gsap.registerPlugin(ScrollTrigger);
+
+window.addEventListener('load', () => {
+    const horizontalSections = gsap.utils.toArray(".horiz-gallery-wrapper");
+
+    horizontalSections.forEach((sec) => {
+        const strip = sec.querySelector(".horiz-gallery-strip");
+
+        gsap.to(strip, {
+            x: () => -(strip.scrollWidth - window.innerWidth),
+            ease: "none",
+            scrollTrigger: {
+                trigger: sec, // which element starts the effect
+                pin: true, // locks the section in place while scrolling
+                scrub: 1, // ties animation to scroll position (1 = slight lag/smoothness)
+                start: "center center", // starts when top of section hits top of viewport
+                end: () => `+=${strip.scrollWidth}`, // ends after scrolling the full width of the strip
+                invalidateOnRefresh: true // recalculates values if window is resized
+            }
+        });
+    });
+
+    // Force ScrollTrigger to recalculate after load
+    ScrollTrigger.refresh();
+
+});
